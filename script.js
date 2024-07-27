16aa75eefaff94d22ff11a89e87c21ece16566a4
Asaduzzaman04 <asadchy152@gmail.com>
2024-07-27 19:26:51 +0600

    add ripple effect

---
+++ script.js
@@ -0,0 +1,42 @@
+const btn1 = document.getElementById("link1");
+const btn2 = document.getElementById("link2");
+const btn3 = document.getElementById("link3");
+const btn4 = document.getElementById("link4");
+
+// =====>first-button <=====
+btn1.addEventListener("mouseover", (e) =>{
+    const x = e.pageX - btn1.offsetLeft;
+    const y = e.pageY - btn1.offsetTop;
+    
+    btn1.style.setProperty("--pageL", x + 'px');
+    btn1.style.setProperty("--pageY", y + 'px');
+})
+// =====>second-button <=====
+
+btn2.addEventListener("mouseover", (e) =>{
+    const x = e.pageX - btn2.offsetLeft;
+    const y = e.pageY - btn2.offsetTop;
+    
+    btn2.style.setProperty("--pageL", x + 'px');
+    btn2.style.setProperty("--pageY", y + 'px');
+})
+
+// =====>third-button <=====
+btn3.addEventListener("mouseover", (e) =>{
+    const x = e.pageX - btn3.offsetLeft;
+    const y = e.pageY - btn3.offsetTop;
+    
+    btn3.style.setProperty("--pageY", y + 'px');
+    btn3.style.setProperty("--pageL", x + 'px');
+})
+
+// =====>fourth-button <=====
+btn4.addEventListener("mouseover", (e) =>{
+    const x = e.pageX - btn4.offsetLeft;
+    const y = e.pageY - btn4.offsetTop;
+
+    btn4.style.setProperty("--pageL", x + 'px');
+    btn4.style.setProperty("--pageY", y + 'px');
+})
+
+// sticky-nav-bar-section \\
